b234fc0abd9759c37be45af008381fef5534dc27
marciioluucas <marciioluucas@gmail.com>
2017-04-10 13:58:10 -0300

    Colocando ajax nas paradas das perguntas.

--- view/indexDashComum.php
+++ view/indexDashComum.php
@@ -9,7 +9,7 @@ $usuarioController = new UsuarioController();
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Área do cliente | CataLOG</title>
+    <title>Área do <?php echo $usuarioController->retornaAlgoUsuario("nome", $idUser); ?> | Chemistery</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
